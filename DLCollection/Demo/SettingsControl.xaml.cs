fdd2b84eca37774df9aba54a2f13429f9185c6d0
Arseniy Kucher <beeffallo@gmail.com>
2017-07-20 21:32:56 +0300

    work on settings UI

--- DLCollection/Demo/SettingsControl.xaml.cs
+++ DLCollection/Demo/SettingsControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,19 +24,107 @@ namespace DLCollection.Demo
             InitializeComponent();
             Loaded += (s, e) =>
             {
-                brdSettings.Visibility = Visibility.Visible;
-                cntSourceSelector.Visibility = Visibility.Collapsed;
-                cntApplySettings.Visibility = Visibility.Collapsed;
-                cntDetectMethodParams.Visibility = Visibility.Collapsed;
+                ExpandSettings();
             };
         }
 
         private void CmdSettings_Click(object sender, RoutedEventArgs e)
         {
-            brdSettings.Visibility = Visibility.Collapsed;
+            ExpandSettings();
+        }
+
+        private void CollapseSettings()
+        {
+            cmdOpenSettings.Visibility = Visibility.Visible;
+            cntSourceSelector.Visibility = Visibility.Collapsed;
+            cntApplySettings.Visibility = Visibility.Collapsed;
+            cntDetectMethodParams.Visibility = Visibility.Collapsed;
+            tbSettingsTitle.Visibility = Visibility.Collapsed;
+        }
+
+        private void ExpandSettings()
+        {
+            _tempSettings = Settings;
+            cmdOpenSettings.Visibility = Visibility.Collapsed;
             cntSourceSelector.Visibility = Visibility.Visible;
             cntApplySettings.Visibility = Visibility.Visible;
             cntDetectMethodParams.Visibility = Visibility.Visible;
+            tbSettingsTitle.Visibility = Visibility.Visible;
         }
+
+        private void CmdApply_Click(object sender, RoutedEventArgs e)
+        {
+            Settings = _tempSettings;
+            CollapseSettings();
+        }
+
+        private void CmdD0_Click(object sender, RoutedEventArgs e)
+        {
+            _tempSettings.CascadeType = SettingsControlProperties.Cascade.Default;
+            cmdD0.Style = FindResource("MainButtonInverseStyle") as Style;
+            cmdA1.Style = FindResource("MainButtonStyle") as Style;
+            cmdA2.Style = FindResource("MainButtonStyle") as Style;
+        }
+
+        private void CmdA1_Click(object sender, RoutedEventArgs e)
+        {
+            _tempSettings.CascadeType = SettingsControlProperties.Cascade.Alt1;
+            cmdA1.Style = FindResource("MainButtonInverseStyle") as Style;
+            cmdD0.Style = FindResource("MainButtonStyle") as Style;
+            cmdA2.Style = FindResource("MainButtonStyle") as Style;
+        }
+
+        private void CmdA2_Click(object sender, RoutedEventArgs e)
+        {
+            _tempSettings.CascadeType = SettingsControlProperties.Cascade.Alt2;
+            cmdA2.Style = FindResource("MainButtonInverseStyle") as Style;
+            cmdD0.Style = FindResource("MainButtonStyle") as Style;
+            cmdA1.Style = FindResource("MainButtonStyle") as Style;
+        }
+
+        private void CmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            CollapseSettings();
+        }
+
+        private void CmdBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            _tempSettings.SourceType = SettingsControlProperties.Source.Browse;
+            _tempSettings.BrowsePath = BrowseDialog();
+            if (string.IsNullOrEmpty(_tempSettings.BrowsePath)) return;
+
+            cmdBrowse.Style = FindResource("MainButtonInverseStyle") as Style;
+            cmdBrowseIcon.Content = FindResource("ImageIconWhite") as Path;
+            cmdBrowseText.Style = FindResource("MainTextBlockInverseStyle") as Style;
+
+            cmdCapture.Style = FindResource("MainButtonStyle") as Style;
+            cmdCaptureIcon.Content = FindResource("TakePhotoIconBlack") as Path;
+            cmdCaptureText.Style = FindResource("MainTextBlockStyle") as Style;
+        }
+
+        private string BrowseDialog()
+        {
+            var dialog = new CommonOpenFileDialog();
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Cancel || result == CommonFileDialogResult.None) return null;
+            if (result == CommonFileDialogResult.Ok) return dialog.FileName;
+            return null;
+        }
+
+        private void CmdCapture_Click(object sender, RoutedEventArgs e)
+        {
+            _tempSettings.SourceType = SettingsControlProperties.Source.Capture;
+
+            cmdCapture.Style = FindResource("MainButtonInverseStyle") as Style;
+            cmdCaptureIcon.Content = FindResource("TakePhotoIconWhite") as Path;
+            cmdCaptureText.Style = FindResource("MainTextBlockInverseStyle") as Style;
+
+            cmdBrowse.Style = FindResource("MainButtonStyle") as Style;
+            cmdBrowseIcon.Content = FindResource("ImageIconBlack") as Path;
+            cmdBrowseText.Style = FindResource("MainTextBlockStyle") as Style;
+        }
+
+        public SettingsControlProperties Settings { get; set; } = new SettingsControlProperties();
+        private SettingsControlProperties _tempSettings;
     }
 }
---
+++ DLCollection/Demo/SettingsControlProperties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLCollection.Demo
+{
+    public class SettingsControlProperties
+    {
+        public enum Cascade { None, Default, Alt1, Alt2 }
+        public Cascade CascadeType { get; set; } = Cascade.None;
+
+        public enum Source { None, Capture, Browse }
+        public Source SourceType { get; set; } = Source.None;
+        public string BrowsePath { get; set; }
+
+        public double ScaleFactor { get; set; }
+        public int MinNeighbors { get; set; }
+
+        public Size MinSize { get; set; }
+        public Size MaxSize { get; set; }
+    }
+}
